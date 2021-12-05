d865caa5ffeb529fa21b8457915ff08343c77693
Johan <grimmerw968@gmail.com>
2021-12-05 10:58:56 +0200

    feat: add typescript to news file

---
+++ migration-to-typescript/src/components/view/news/news.ts
@@ -0,0 +1,40 @@
+import './news.css';
+import { SourceArticles } from '../../template-types';
+
+class News {
+    draw(data: SourceArticles[]) {
+        console.log(data);
+        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment() as DocumentFragment;
+        const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
+
+        news.forEach((item, idx) => {
+            const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement;
+
+            if (idx % 2) (newsClone.querySelector('.news__item') as HTMLElement).classList.add('alt');
+
+            (<HTMLElement>newsClone.querySelector('.news__meta-photo')).style.backgroundImage = `url(${
+                item.urlToImage || 'img/news_placeholder.jpg'
+            })`;
+            (<HTMLElement>newsClone.querySelector('.news__meta-author')).textContent = item.author || item.source.name;
+            (<HTMLElement>newsClone.querySelector('.news__meta-date')).textContent = item.publishedAt
+                .slice(0, 10)
+                .split('-')
+                .reverse()
+                .join('-');
+
+            (<HTMLElement>newsClone.querySelector('.news__description-title')).textContent = item.title;
+            (<HTMLElement>newsClone.querySelector('.news__description-source')).textContent = item.source.name;
+            (<HTMLElement>newsClone.querySelector('.news__description-content')).textContent = item.description;
+            (<HTMLElement>newsClone.querySelector('.news__read-more a')).setAttribute('href', item.url);
+
+            fragment.append(newsClone);
+        });
+
+        (document.querySelector('.news') as HTMLElement).innerHTML = '';
+        (document.querySelector('.news') as HTMLElement).appendChild(fragment);
+    }
+}
+
+export default News;

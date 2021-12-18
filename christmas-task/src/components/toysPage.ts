3dec174497102beac576c713bebf8fe043459c8d
Johan <grimmerw968@gmail.com>
2021-12-18 18:26:19 +0200

    feat: add local storage create funtion

--- christmas-task/src/components/toysPage.ts
+++ christmas-task/src/components/toysPage.ts
@@ -1,7 +1,23 @@
 import { JSONData } from '../components/inteface/templayTypes';
+import noUiSlider from 'nouislider';
+/* import '../../node_modules/nouislider/distribute/nouislider.css'; */
 
 export class ToysPage {
     constructor() {}
+    private createLocalStorage() {
+        const setLocalStorage = () => {
+            const objFavoritesToys = {};
+            const saveSettingObj = {};
+
+            if (localStorage.getItem('objFavoritesToys') === null) {
+                localStorage.setItem('objFavoritesToys', JSON.stringify(objFavoritesToys));
+            }
+            if (localStorage.getItem('saveSettingObj') === null) {
+                localStorage.setItem('saveSettingObj', JSON.stringify(saveSettingObj));
+            }
+        };
+        window.addEventListener('load', setLocalStorage);
+    }
     private prerenderSlider() {}
     public prerender() {
         const generateArray = [...Array(60).keys()].map((i) => i + 1);
@@ -29,7 +45,6 @@ export class ToysPage {
             const url = await key;
             arrayImg.push(url);
         }
-
         const dataHTML = dataJSON.then((value: any) => {
             return value.map((el: any, i: number) => {
                 return `
@@ -71,56 +86,3 @@ export class ToysPage {
         dataHTML.then((data: any) => htmlTemplate?.insertAdjacentHTML('afterbegin', data.join('')));
     }
 }
-
-/* 
-public async createToysBox(urls: Promise<string>[], dataJSON: any) {
-    const htmlTemplate = document.querySelector('.toys__inner');
-    const arrayImg: string[] = [];
-
-    for (const key of urls) {
-        const url = await key;
-        arrayImg.push(url);
-    }
-
-    dataJSON.then((value: any) => {
-        for (let i = 0; i <= value.length; i++) {
-            htmlTemplate?.insertAdjacentHTML(
-                'afterbegin',
-                `
-                <div class="toys__box">
-                    <h3 class="toys__name">${value[i].name}</h3>
-                        <div class="toys__wrapper">
-                            <div class="toys__description">
-                                <div class="toys__number">
-                                    Количество <span class="toys__amount">${value[i].count}</span>
-                                </div>
-                                <div class="toys__year">
-                                    Год покупки: <span class="year__number">${value[i].year}</span>
-                                </div>
-                                <div class="toys__form">
-                                    Форма: <span class="form__view">${value[i].shape}</span>
-                                </div>
-                                <div class="toys__color">
-                                    Размер: <span class="color__view">${value[i].color}</span>
-                                </div>
-                                <div class="toys__size">
-                                    Размер: <span class="size__view">${value[i].size}</span>
-                                </div>
-                                <div class="toys__liked">
-                                    Любимая: <span class="liked__toy">${value[i].favorite}</span>
-                                </div>
-                                </div>
-                                <div class="toys__img">
-                                    <img
-                                    class="toys__img__garland"
-                                    src="${arrayImg[i]}"
-                                        alt="toy garland"
-                                         />
-                                </div>
-                                 </div>
-                            </div>
-            `
-            );
-        }
-    });
-} */

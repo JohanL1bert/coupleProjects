35d1a3798dfa7b556d112c8b55d33377f920c96f
Johan <grimmerw968@gmail.com>
2021-12-18 18:23:33 +0200

    feat: start task

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,1 +1,16 @@
 import './css/style.scss';
+import { ToysPage } from './components/toysPage';
+import { ToysSettingFilter } from './components/toysSettings';
+
+const run = () => {
+    const toysArray = new ToysPage();
+    const arrayImg = toysArray.prerender();
+    const dataJSON = toysArray.getData();
+    toysArray.createToysBox(arrayImg, dataJSON);
+};
+
+run();
+
+const newSetting = new ToysSettingFilter();
+
+newSetting.cycleSettings();

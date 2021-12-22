c2219a3dc4186f25d3456da6ab1a316cb191d3ba
Johan <grimmerw968@gmail.com>
2021-12-22 15:26:00 +0200

    refactor: remove text info

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,12 +2,9 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super(
-            'https://nodenews.herokuapp.com/', //Изначально бралось отсюда 'https://newsapi.org/v2/', но так работает только локально
-            {
-                apiKey: /* '7f4f7f705d514982ac0b5f707bf3f827' */ '9c6f2bdddb0741d494c4eb2d6886c076', // получите свой ключ https://newsapi.org/
-            }
-        );
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: '9c6f2bdddb0741d494c4eb2d6886c076',
+        });
     }
 }
 

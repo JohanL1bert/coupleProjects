ba456dfc0f87baafb8f1511f6673172ab28fa9ad
Johan <grimmerw968@gmail.com>
2021-12-27 21:37:19 +0200

    feat: add interfaces

---
+++ christmas-task/src/components/interface/templayTypes.ts
@@ -0,0 +1,34 @@
+interface JSON {
+    num: string;
+    name: string;
+    year: string;
+    shape: string;
+    color: string;
+    size: string;
+    favorite: boolean;
+}
+
+export interface JSONData {
+    [key: string]: Array<JSON>;
+}
+
+export type Callback<T> = (data: T) => void;
+
+export interface SettingObject {
+    [key: string]: boolean;
+}
+
+export interface SortObject {
+    [key: string]: string;
+}
+
+export interface rangeObject {
+    min: number;
+    max: number;
+}
+
+export interface ObjectValue {
+    shape: string;
+    color: string;
+    size: string;
+}

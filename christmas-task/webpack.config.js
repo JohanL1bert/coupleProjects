6022af62129ff3371cc463e6852a9724692781bd
Johan <grimmerw968@gmail.com>
2021-12-27 21:36:08 +0200

    feat: add webpack config

--- christmas-task/webpack.config.js
+++ christmas-task/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;

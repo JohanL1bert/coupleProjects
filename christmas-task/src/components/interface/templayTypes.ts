a1f55dd8de88230a640d185a0355f33203beca95
Johan <grimmerw968@gmail.com>
2022-01-02 14:10:39 +0200

    feat: interfaces to data and enum to sort

--- christmas-task/src/components/interface/templayTypes.ts
+++ christmas-task/src/components/interface/templayTypes.ts
@@ -43,3 +43,27 @@ export interface IsliderCount {
     min: number;
     max: number;
 }
+
+export interface IshapeColorSize {
+    shape: string[];
+    color: string[];
+    size: string[];
+}
+
+export interface IdataMain {
+    color: string;
+    count: string;
+    favorite: boolean;
+    name: string;
+    num: string;
+    shape: string;
+    size: string;
+    year: string;
+}
+
+export enum EsortedValue {
+    SortNameMax = 'sort-name-max',
+    SortNameMin = 'sort-name-min',
+    SortMax = 'sort-max',
+    SortMin = 'sort-min',
+}

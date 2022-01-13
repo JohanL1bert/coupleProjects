f93f2bc8581c40750957cc28b5b0070a819e0069
Johan <grimmerw968@gmail.com>
2022-01-13 16:24:28 +0200

    feat: add settings for project

---
+++ async-race/src/index.ts
@@ -0,0 +1,2 @@
+import './components/garage/garage.sass';
+import './components/winners/winners.sass';
---
+++ async-race/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const fileName = (ext) => (isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`);
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: isDev ? 'development' : 'production',
+    devtool: isDev ? 'inline-source-map' : false,
+    entry: './index.ts',
+    output: {
+        filename: `${fileName('js')}`,
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: `assets/[name][ext]`,
+        clean: true,
+    },
+    devServer: {
+        open: true,
+        port: 8000,
+        watchFiles: ['./src/index.html'],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `[name].css`,
+        }),
+        new HTMLWebpackPlugin({
+            title: 'async-race',
+            template: path.resolve(__dirname, `./src/index.html`),
+            filename: `index.html`,
+            minify: {
+                collapseWhitespace: isProd,
+            },
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.[tj]s$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.mp3$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+};

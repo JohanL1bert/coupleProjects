3296749b009043c05815dbb503467296e3b0fa17
Johan <grimmerw968@gmail.com>
2022-01-18 18:52:50 +0200

    refactor: rename class name and create class to listeners

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -2,11 +2,37 @@ import './components/scssSettings/fonts.scss';
 import './components/garage/garage.scss';
 import './components/winners/winners.scss';
 import './global.scss';
-import { App } from './components/routing';
+import { Router } from './components/routing';
 import { Garage } from './components/garage/garage';
+import { Winners } from './components/winners/winners';
 
-const newApp: App = new App();
+class App {
+    router: Router;
+    garagePage: Garage;
+    winnersPage: Winners;
+    constructor(router: Router, garagePage: Garage, winnersPage: Winners) {
+        this.router = router;
+        this.garagePage = garagePage;
+        this.winnersPage = winnersPage;
+    }
+
+    public root() {
+        this.router.root();
+        this.renderPage();
+        this.garagePage.initialEmptyGarage();
+    }
+
+    public renderPage() {
+        const btnGaragePage = this.router.getHTMLElement('navigation__garage');
+        const bntWinnerPage = this.router.getHTMLElement('navigation__winners');
+
+        btnGaragePage.addEventListener('click', this.garagePage.initialEmptyGarage.bind(this.garagePage));
+        //bntWinnerPage.addEventListener('click', this.garagePage.initialEmptyGarage.bind(this.winnersPage));
+    }
+}
+
+const newApp: Router = new Router();
 const garage: Garage = new Garage(newApp);
-
-newApp.root();
-garage.initialEmptyGarage();
+const winners: Winners = new Winners(newApp);
+const app = new App(newApp, garage, winners);
+app.root();

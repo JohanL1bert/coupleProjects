e24efa97fa89aadee701b6efeb80f565d8b5b22d
Johan <grimmerw968@gmail.com>
2022-01-18 15:30:59 +0200

    feat:  add root project

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,2 +1,12 @@
-import './components/garage/garage.sass';
-import './components/winners/winners.sass';
+import './components/scssSettings/fonts.scss';
+import './components/garage/garage.scss';
+import './components/winners/winners.scss';
+import './global.scss';
+import { App } from './components/routing';
+import { Garage } from './components/garage/garage';
+
+const newApp: App = new App();
+const garage: Garage = new Garage(newApp);
+
+newApp.root();
+garage.initialEmptyGarage();

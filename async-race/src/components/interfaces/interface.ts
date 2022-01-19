3dfbd0a82c709f6275c6836bad2b73aa2e3d03ae
Johan <grimmerw968@gmail.com>
2022-01-19 15:12:49 +0200

    feat: add type to page garage

---
+++ async-race/src/components/interfaces/interface.ts
@@ -0,0 +1,19 @@
+export type TArrayClassName = Array<string[]>;
+
+export type TGarageSet = [HTMLElement, HTMLInputElement, HTMLInputElement, HTMLButtonElement];
+
+export type TCarItem = [
+    HTMLElement,
+    HTMLElement,
+    HTMLButtonElement,
+    HTMLButtonElement,
+    HTMLElement,
+    HTMLElement,
+    HTMLElement,
+    HTMLButtonElement,
+    HTMLButtonElement,
+    HTMLElement,
+    HTMLElement,
+    HTMLElement,
+    HTMLElement
+];
